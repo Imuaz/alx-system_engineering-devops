4e67135cd0e58b449b69ccf98eca718c82628cc8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-30 10:26:07 +0000

    readme review and correction

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Puppet script that sets up a client's SSH configuration file so that he can
+# connect to a server without typing a password.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
