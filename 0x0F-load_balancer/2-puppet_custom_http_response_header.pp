011525eee6bedf5f0d68bef7cba4f6982815b5b2
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-31 11:33:22 +0000

    readme review and correction

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,16 @@
 # Use Puppet to automate the task of creating a custom HTTP header response
 
-exec { 'update':
+exec {'update':
   command => '/usr/bin/apt-get update',
-  logoutput => true,
 }
-
-package { 'nginx':
-  ensure => 'installed',
-  provider => 'apt',
-  require => Exec['update'],
-}
-
-file { '/var/www/html/index.html':
-  ensure => 'file',
-  content => 'Hello World!',
-}
-
-exec { 'allow_nginx':
-  command => '/usr/sbin/ufw allow "Nginx HTTP"',
-  unless => '/usr/sbin/ufw status | grep "Nginx HTTP"',
-}
-
-file_line { 'redirect':
+-> package {'nginx':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  notify => Service['nginx'],
 }
-
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
