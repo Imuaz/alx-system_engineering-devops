bb0dbb6a1a0069ec0757a960f8be9791029a18c8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-01 13:14:53 +0100

    readme review and correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Puppet manifest that creates a file in `/tmp`.
 file { '/tmp/school':
-  ensure =>  file,
-  mode =>  '0744',
-  owner =>  'www-data',
-  group =>  'www-data',
-  content =>  'I love Puppet',
+      ensure  => file,
+      mode    => '0774',
+      owner   => 'www-data',
+      group   => 'www-data',
+      content => 'I love Puppet',
 }
