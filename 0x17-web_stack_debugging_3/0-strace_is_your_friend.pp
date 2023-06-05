792ed11f327d849422bd265a2e172aca194db598
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-05 09:01:29 +0000

    readme review and correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'fix typo':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless  => '[ -e /var/www/html/wp-settings.php ] && grep -q "phpp" /var/www/html/wp-settings.php',
+  unless  => 'test ! -e /var/www/html/wp-settings.php || grep -q "phpp" /var/www/html/wp-settings.php',
 }
