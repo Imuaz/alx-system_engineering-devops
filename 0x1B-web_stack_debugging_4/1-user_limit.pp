729d1da530bbe1c4f663960884cbd020c82a7e2b
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-05 20:57:04 +0000

    readme review and correction

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,9 @@
 # Enable the user holberton to login and open files without error
-file { '/etc/security/limits.conf':
-  ensure  =>  present,
-  content =>  template('module/limits.conf.erb'),
+exec {'Updating limits to Holberton User':
+  command  => "sed -i 's/holberton soft nofile.*/holberton soft nofile 9000/' /etc/security/limits.conf",
+  provider => shell
 }
-
-exec { 'update-limits-for-holberton-user':
-  command     =>  "sed -i -e 's/holberton soft nofile.*/holberton soft nofile \
-  9000/' -e 's/holberton hard nofile.*/holberton hard nofile 9000/' \
-  /etc/security/limits.conf",
-  path        =>  '/usr/local/bin:/usr/bin:/bin',
-  refreshonly =>  true,
-  subscribe   =>  File['/etc/security/limits.conf'],
+exec {'Updating limit to Holberton User':
+  command  => "sed -i 's/holberton hard nofile.*/holberton hard nofile 9000/' /etc/security/limits.conf",
+  provider => shell
 }
