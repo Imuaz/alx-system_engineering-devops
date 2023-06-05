8b9b20dd43858f36cd0b0f43f108a2600bbcc50a
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-05 09:04:55 +0000

    readme review and correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # fixes an Apache webser returning 500 error by fixing typo in wordpress
 exec { 'fix typo':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless  => 'test ! -e /var/www/html/wp-settings.php || grep -q "phpp" /var/www/html/wp-settings.php',
+  command     => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
 }
