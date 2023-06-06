52b57c491054da601267cbe48f6cc94a3c19775e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 15:05:24 +0000

    readme review and correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
 """Exports data in the CSV format"""
 
-import csv
 import requests
 import sys
+import csv
 
-if __name__ == "__main__":
-    userId = sys.argv[1]
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
-    name = user.json().get('username')
-    todos = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={userId}")
+if __name__ == '__main__':
+    if len(sys.argv) <= 1 or not sys.argv[1].isdigit():
+        sys.exit(0)
 
-    filename = f"{userId}.csv"
-    with open(filename, mode='w', newline='') as f:
+    BASE_API = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response_url = f"{BASE_API}users/{employee_id}"
+    todo_response_url = f"{BASE_API}users/{employee_id}/todos"
+
+    user_response = requests.get(user_response_url).json()
+    todo_response = requests.get(todo_response_url).json()
+
+    employee_name = user_response.get('name')
+    username = user_response.get('username')
+
+    filename = f"{employee_id}.csv"
+    with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
                          "TASK_TITLE"])
-        for task in todos.json():
-            completed_status = str(task.get('completed'))
-            task_title = task.get('title')
-            writer.writerow([userId, name, completed_status, task_title])
+        for todo in todo_response:
+            task_status = str(todo.get("completed"))
+            task_title = todo.get("title")
+            writer.writerow([employee_id, username, task_status, task_title])
 
     print(f"Data exported to {filename}.")
