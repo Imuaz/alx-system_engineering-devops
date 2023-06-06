b0432d4a8f17f46c1a199b1cdf3e54222b016362
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 23:18:05 +0000

    readme review and correction

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Task 2 of Advanced APIs Module
+"""
+import requests
+import sys
+
+
+def recurse(subreddit, hot_list=[], after=None,):
+    """Queries Reddit API and prints the titles of all hot posts listed
+    for a given subreddit."""
+
+    headers = {"User-Agent": "My-User-Agent"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {"limit": 100, "after": after}
+
+    response = requests.get(
+        url=url,
+        headers=headers,
+        params=params,
+        allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        children = data['children']
+        hot_list.extend([child['data']['title']
+                         for child in children])
+        after = response.json()['data']['after']
+        if not after:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        subred = sys.argv[1]
+        recurse(subred, [])
