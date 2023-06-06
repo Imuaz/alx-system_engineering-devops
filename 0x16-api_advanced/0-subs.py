81c14573b8820a68914b5b2b4b14a799a86ae73b
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 18:11:50 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """Module for task 0"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """
+    Queries the Reddit API and returns the number of subscribers
+    to the subreddit.
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    Args:
+    subreddit (str): The name of the subreddit.
+    Returns:
+    int: The number of subscribers to the subreddit. Returns 0
+    if the subreddit is invalid or an error occurs
+    during the API request.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    data = response.json().get("data")
+    subscribers = data.get("subscribers")
+    return subscribers
