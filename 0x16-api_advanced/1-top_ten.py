72b781f9340ea9484b065f7c293353941980e33d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 23:11:02 +0000

    readme review and correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     """Queries Reddit API and prints the titles of the first 10 hot
     posts listed for a given subreddit."""
 
-    headers = {"User-Agent": "user_agent"}
+    headers = {"User-Agent": "My-User-Agent"}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
     response = requests.get(url, headers=headers, allow_redirects=False)
