dcf8f36f8681876c7910e83163b450f0bb509c0f
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 22:38:34 +0000

    readme review and correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-Module task 1
+Task 1 of Advanced API Module
 """
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """Function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-
+    """
+    queries Reddit API and prints the titles of the first 10 hot posts
+    for a given subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}  # User-Agent header
+    headers = {"User-Agent": "MyRedditClient"}  # User-Agent header
 
     try:
         response = requests.get(url, headers=headers)
