f49ef78eab43099fe878d25b23cafa64e8fc290c
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 18:16:18 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,8 @@
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    to the subreddit.
-
-    Args:
-    subreddit (str): The name of the subreddit.
-    Returns:
-    int: The number of subscribers to the subreddit. Returns 0
-    if the subreddit is invalid or an error occurs
-    during the API request.
-    """
+    """Queries the Reddit API and returns the number of subscribers to
+    the subreddit."""
     import requests
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
