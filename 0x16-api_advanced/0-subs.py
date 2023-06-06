061284819313be153f8018e42e13a27b5a8fe13d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 17:58:40 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
 """Advanced Apis Module for task 0"""
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    headers = {"User-Agent": "My-User-Agent"}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    to the subreddit.
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        subscribers = response.json()["data"]["subscribers"]
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers to the subreddit.
+        Returns 0 if the subreddit is invalid
+            or an error occurs during the API request.
+    """
+    headers = {"User-Agent": "user_agent"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        subscribers = data.get('subscribers')
         return subscribers
-    except (requests.RequestException, KeyError):
+    else:
         return 0
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print(
+            f"The subreddit '{subreddit}' has {num_subscribers} subscribers.")
