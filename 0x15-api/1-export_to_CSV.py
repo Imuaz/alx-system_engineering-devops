f6a14c0827cb75d06e938cf26c22bcbab587a9a0
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 15:09:03 +0000

    readme review and correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-"""Exports data in the CSV format"""
-
-import requests
-import sys
+"""
+Exports data in the CSV format
+"""
 import csv
+import requests
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1 or not sys.argv[1].isdigit():
-        sys.exit(0)
-
-    BASE_API = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
-    user_response_url = f"{BASE_API}users/{employee_id}"
-    todo_response_url = f"{BASE_API}users/{employee_id}/todos"
-
-    user_response = requests.get(user_response_url).json()
-    todo_response = requests.get(todo_response_url).json()
-
-    employee_name = user_response.get('name')
-    username = user_response.get('username')
-
-    filename = f"{employee_id}.csv"
-    with open(filename, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                         "TASK_TITLE"])
-        for todo in todo_response:
-            task_status = str(todo.get("completed"))
-            task_title = todo.get("title")
-            writer.writerow([employee_id, username, task_status, task_title])
-
-    print(f"Data exported to {filename}.")
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        uid)
+    todo = requests.get(url, verify=False).json()
+    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo:
+            taskwriter.writerow([int(uid), user.get('username'),
+                                 t.get('completed'),
+                                 t.get('title')])
