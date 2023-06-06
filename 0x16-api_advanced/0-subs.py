28be21413ae78b835d5289fdbbda683873583d9c
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 18:22:30 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
+    """
+    Queries the Reddit API and returns the number of subscribers
+    to the subreddit
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
