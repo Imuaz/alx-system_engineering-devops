a40f1e8403fa0fce77ff45bcbc0bdb4299a9256e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 18:27:41 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,21 +3,16 @@
 Module task 0
 """
 
-import requests
-
-
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    to the subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My-User-Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Queries Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            header={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if response.status_code >= 300:
         return 0
 
-    data = response.json().get("data")
-    subscribers = data.get("subscribers")
-    return subscribers
+    return response.json().get("data").get("subscribers")
