723f24628400d468449791095b94e92a19411ac0
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 18:33:25 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    response = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            header={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if response.status_code >= 300:
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    return response.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
