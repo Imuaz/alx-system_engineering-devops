9ed7668f59d6c8be16db83636fb74e37c11d6ce2
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 22:47:31 +0000

    readme review and correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""Task 1 of Advanced API Module"""
+"""
+Task 1 Advanced APIs Module
+"""
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """
-    queries Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "MyRedditClient"}  # User-Agent header
+    """Queries Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit."""
 
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Check for request errors
-
-        data = response.json()
-        children = data["data"]["children"]
+    headers = {"User-Agent": "user_agent"}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(
+        url=url,
+        headers=headers,
+        allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()['data']
+        children = data['children']
         for child in children:
-            title = child["data"]["title"]
+            title = child['data']['title']
             print(title)
+    else:
+        print("None")
 
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) > 1:
-        subreddit = sys.argv[1]
-        top_ten(subreddit)
+        subred = sys.argv[1]
+        top_ten(subred)
