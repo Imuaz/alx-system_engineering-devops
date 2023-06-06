1a0c43b28a8810fcf7aad40f46aa261ca530dbe4
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-06 17:35:15 +0000

    readme review and correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
+import requests
+
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """Queries the Reddit API and returns the number of subscribers to
+    the subreddit.
 
-    headers = {"User-Agent": "My-User-Agent"}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    Args:
+        subreddit (str): The name of the subreddit.
 
+    Returns:
+        int: The number of subscribers to the subreddit.
+        Returns 0 if the subreddit doesn't exist
+            or an error occurs while making the API request.
+
+    """
     try:
+        headers = {"User-Agent": "My-User-Agent"}
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
         response = requests.get(url, headers=headers, allow_redirects=False)
         response.raise_for_status()
-        subscribers = response.json()["data"]["subscribers"]
+        subscribers = response.json().get("data").get("subscribers")
         return subscribers
     except (requests.RequestException, KeyError):
         return 0
