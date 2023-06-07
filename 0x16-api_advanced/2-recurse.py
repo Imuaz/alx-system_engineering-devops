d1c5236b876a6fd732a696d257c5a3b3be5a6148
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-07 00:35:18 +0000

    readme review and correction

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -22,7 +22,7 @@ def recurse(subreddit, hot_list=[], after=None,):
 
     if response.status_code == 200:
         data = response.json().get('data')
-        posts = data.get('children')
+        posts = data.get('posts')
         hot_titles = [post.get('data').get('title') for post in posts]
         next_page = data.get('after')
         if not next_page:
