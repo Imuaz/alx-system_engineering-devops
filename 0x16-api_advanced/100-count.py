39376557ee65e84e14bde6362bc07e71d339e244
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-06-07 12:49:57 +0000

    readme review and correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,49 +3,54 @@
 Task 3 Advanced API Module
 """
 import requests
-import sys
+import re
 
 
-def count_words(subreddit, wordlist, hot_list=[], after=None,):
+def count_words(subreddit, word_list, after=None, count_dict=None):
     """Queries Reddit API, parses the titles of all hot articles, and prints a
     sorted count of given keywords (case-insensitive, delimited by spaces."""
-    headers = {"User-Agent": "user_agent"}
-    url = "https://www.reddit.com/r/{}/hot.json".format(
-        subreddit)
-    params = {"limit": 100, "after": after}
-
-    response = requests.get(
-        url=url,
-        headers=headers,
-        params=params,
-        allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        children = data['children']
-        hot_list.extend([child['data']['title']
-                         for child in children])
-        after = response.json()['data']['after']
-        if not after:
-            wordcounts = {}
-            search_str = " ".join(hot_list)
-            wordcounts = {word.lower(): search_str.lower().count(
-                word.lower()) for word in wordlist}
-            sorted_wordcounts = dict(
-                sorted(wordcounts.items(),
-                       key=lambda item: (-item[1], item[0])))
-            for key, value in sorted_wordcounts.items():
-                if value != 0:
-                    print("{}: {}".format(key, value))
-            return (0)
-        return count_words(subreddit, wordlist, hot_list, after)
+    if count_dict is None:
+        count_dict = {}
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     else:
-        return None
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return
+
+    data = response.json()["data"]
+    after = data.get("after")
+    children = data.get("children")
+
+    for child in children:
+        title = child["data"]["title"].lower()
+        for word in word_list:
+            if word.lower() in title and not re.search(
+                    r"\b{}\b".format(word), title):
+                count_dict[word] = count_dict.get(word, 0) + title.count(
+                    word.lower())
+
+    if after:
+        return count_words(subreddit, word_list, after, count_dict)
+
+    sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
+
+    for word, count in sorted_counts:
+        print(f"{word}: {count}")
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 0:
-        subred = sys.argv[1]
-        if (len(sys.argv) > 1):
-            wordlist = sys.argv[2].split()
-        count_words(subred, wordlist, [])
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(
+            sys.argv[0]))
+    else:
+        subreddit = sys.argv[1]
+        word_list = sys.argv[2:]
+        count_words(subreddit, word_list)
